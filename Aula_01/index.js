ac4b2052cbbb0027ed762b557a69ee2a521d64ba
Lucas Andrade <lucasdmandrade@gmail.com>
2023-02-15 21:59:44 -0300

    Segunda aula

--- Aula_01/index.js
+++ Aula_01/index.js
@@ -4,4 +4,16 @@ const express = require("express");
 const app = express();
 
 //Chamar a porta do servidor
-app.listen(8080);
+app.listen(8080, () => console.log("server ativo"));
+
+//Criando a primeira rota
+app.get("/", () => {
+  console.log("Aoba, bão?");
+});
+
+let nome = "Lucas";
+
+//Rota de envio de marcação
+app.get("/testeHTML", (req, res) => {
+  res.send("<h1 style='color: blue'> Ola minha primeira pagina </h1>" + nome);
+});
